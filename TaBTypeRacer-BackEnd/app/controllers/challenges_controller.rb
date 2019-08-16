51ac9b963ee1f916754dfadb397bf340b9c95094
Philip Sterling <flatironschool@Flatirons-MacBook-Air-8.local>
2019-08-16 14:46:40 -0700

    adding basic game functionailty, just state for now

--- TaBTypeRacer-BackEnd/app/controllers/challenges_controller.rb
+++ TaBTypeRacer-BackEnd/app/controllers/challenges_controller.rb
@@ -5,9 +5,19 @@ class ChallengesController < ApplicationController
         render json: @challenge
     end
 
+    def show
+        @challenge = Challenge.find(params[:id])
+        render json: @challenge
+    end
+
     def index
         @challenges = Challenge.all
         render json: @challenges
+    end
+
+    def achall 
+        @challenges = Challenge.all
+        render json: @challenges.sample
     end
 
     private
--- tab-type-racer-front-end/src/containers/GameContainer.js
+++ tab-type-racer-front-end/src/containers/GameContainer.js
@@ -6,33 +6,49 @@ class GameContainer extends React.Component {
   constructor () {
     super()
     this.state={
-      input: ''
+      challenge: '',
+      input: '',
+      index: null,
+      characAt: ""
     }
   }
 
   componentDidMount = () => {
-    //fetch data from challenage
-    // fetch()
-    // .then(resp => resp.json())
-    // .then(data => console.log(data))
+    fetch('http://localhost:7777/challenges/random', {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('jwt')}`
+      }
+    })
+    .then(res => res.json())
+    .then(data => this.setState({
+      challenge: data.paragraph,
+    }))
   }
 
   handleChange = (e) => {
     const input = e.target.value
+    let index = input.length -1
     this.setState({
-      input
+      input,
+      index,
+      characAt: this.state.challenge.charAt(index)
     })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
   }
 
   render() {
     return (
       <div id="newgame-container">
         <Nav />
-        <div className="newgame-container2"> 
+        <div className="newgame-container2">
           <div className="newgame-container3">
-            <form>
-              <p></p>
-              <input type="text" onChange={this.handleChange}/>
+            <form onSubmit={this.handleSubmit}>
+              <p>{this.state.challenge}</p>
+              <input type="text" onChange={this.handleChange} value={this.state.input}/>
             </form>
           </div>
         </div>
